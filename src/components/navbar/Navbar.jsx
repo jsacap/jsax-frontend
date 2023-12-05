16a62afddc5c5900de5404800b3d9d6b5b436c12
jsacap <sanchoalegre@gmail.com>
2023-12-05 16:16:46 +1100

    Navbar Complete with css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
-import './navbar.css'
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import './navbar.css';
+import logo from '../../assets/logo.png';
+
+
+const Menu = () => (
+  <>
+    <p><a href='#'>Home</a></p>
+    <p><a href='#'>Blog</a></p>
+    <p><a href='#'>Trading</a></p>
+    <p><a href='#'>Investor</a></p>
+  </>
+)
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
-    <div>Navbar</div>
+    <div className='jsa__navbar'>
+      <div className='jsa__navbar-links'>
+        <div className='jsa__navbar-links_logo'>
+          <img src={logo} alt='logo'/>
+        </div>
+          <div className='jsa__navbar-links_container'>
+            <Menu />
+          </div>
+      </div>
+    <div className='jsa__navbar-sign'>
+      <p>LOGIN</p>
+      <button type='button'>Register</button>
+    </div>
+    <div className='jsa__navbar-menu'>
+    {toggleMenu}
+    ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)}/>
+    : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)}/>
+    {toggleMenu && (
+      <div className='jsa__navbar-menu_container scale-up-center'>
+        <div className='jsa__navbar-menu_container-links'>
+          <Menu />
+          <div className='jsa__navbar-menu_container-links-sign'>
+      <p>LOGIN</p>
+      <button type='button'>Register</button>
+    </div>
+        </div>
+      </div>
+    )}
+    </div>
+    </div>
   )
 }
 
