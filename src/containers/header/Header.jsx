e90b20ff9ab658b84af9f7ccf19d093710a530c0
jsacap <sanchoalegre@gmail.com>
2023-12-05 19:56:51 +1100

    header complete and trading table started

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './header.css'
+import bull from '../../assets/bull.jpg';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='jsa__header section__padding' id="home">
+      <div className='jsa__header-content'>
+        <h1 className='gradient__text'>JSAX TRADE</h1>
+        <p>UNLOCK THE POWER OF DATA-DRIVEN INSIGHTS BUILT FOR TRADERS BY TRADERS.</p>
+        <p>Enter your trading data, and let our algorithms unearth the hidden gems within. Discover opportunities, refine strategies, and elevate your trading performance.</p>
+        <div className='jsa__header-content__input'>
+          <input type='email' placeholder='Your Email Address'/>
+          <button type='button'>Start</button>
+        </div>
+      </div>
+    <div className='jsa__header-image'>
+      <img src={bull} alt='trading bull' />
+    </div>
+      
+    </div>
   )
 }
 
--- src/containers/trading/Trading.jsx
+++ src/containers/trading/Trading.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-import './trading.css'
+import './trading.css';
+import { Feature } from '../../components/'
 
 const Trading = () => {
   return (
-    <div>Trading</div>
+    <div className='jsa__jsaxtrade section__margin' id="jstrade">
+      <div className="jsa__jsaxtrade-feature">
+        <Feature />
+      </div>
+    <div className='jsa__jsaxtrade-heading'>
+      <h1 className='gradient__text'>Advanced Analytics</h1>
+      <p>Take a deep dive into your trading</p>
+    </div>
+    <div className='jsa__jsaxtrade-container'>
+      <Feature />
+      <Feature />
+      <Feature />
+    </div>
+    </div>
   )
 }
 
