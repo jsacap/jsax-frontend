6d3721fa452b92c4161e4d2735acdbd328295618
jsacap <sanchoalegre@gmail.com>
2023-12-08 06:54:25 +1100

    fixed trading section layout

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,19 +1,16 @@
-import React from 'react'
-import './feature.css'
+import React from 'react';
+import './feature.css';
 
-const Feature = ({ title, text }) => {
-  return (
-    <div className='jsa__features-container'>
-      <div className='jsa__features-container__feature-title'>
+const Feature = ({ title, text }) => (
+  <div className="jsa__features-container__feature">
+    <div className="jsa__features-container__feature-title">
       <div />
       <h1>{title}</h1>
-      </div>
-      <div className='jsa__features-container_feature-text'>
-        <p>{text}</p>
-      </div>
-      
     </div>
-  )
-}
+    <div className="jsa__features-container_feature-text">
+      <p>{text}</p>
+    </div>
+  </div>
+);
 
-export default Feature
+export default Feature;
